eb0f4c93992b6d2a7f2d2f8af7b24f661226af86
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 11:40:20 -0500

    updated the Product Detail component to use and update the global state

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,6 +1,8 @@
 //TODO: PRODUCT DETAIL PAGE COMPONENT
 //! Import dependencies
 import React, { useEffect, useState } from 'react';
+import { useStoreContext } from '../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../utils/actions';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
@@ -8,19 +10,25 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
+  const [state, dispatch] = useStoreContext();
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  const { products } = state;
 
   useEffect(() => {
     if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
+      setCurrentProduct(products.find(product => product._id === id));
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
     }
-  }, [products, id]);
+  }, [products, data, dispatch, id]);
 
   return (
     <>
