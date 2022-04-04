341005c339cbec2c8de707b0de1894d594718478
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 13:50:43 -0500

    added ability for user to manually edit the quantity of shopping cart items by directly typing in the input elements

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -11,6 +11,24 @@ const CartItem = ({ item }) => {
       _id: item._id,
     });
   };
+
+  const onChange = e => {
+    const value = e.target.value;
+
+    if (value === '0') {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id,
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value),
+      });
+    }
+  };
+
   return (
     <div className="flex-row">
       <div>
@@ -22,7 +40,12 @@ const CartItem = ({ item }) => {
         </div>
         <div>
           <span>Qty:</span>
-          <input type="number" placeholder="1" value={item.purchaseQuantity} />
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChange}
+          />
           <span
             role="img"
             aria-label="trash"
