ee3e4ab6fce450d68f41a9d572e46aa67a5918a5
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 13:46:23 -0500

    added ability to add products on the homepage and remove or add products to cart on the detail.js page

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -11,12 +11,27 @@ function ProductItem(item) {
   const { image, name, _id, price, quantity } = item;
   const [state, dispatch] = useStoreContext();
 
+  const { cart } = state;
+
   const addToCart = () => {
-    dispatch({
-      type: ADD_TO_CART,
-      product: { ...item, purchaseQuantity: 1 },
-    });
+    // find the cart item with the matching id
+    const itemInCart = cart.find(cartItem => cartItem._id === _id);
+
+    // if there was a match, call UPDATE with a new purchase quantity
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 },
+      });
+    }
   };
+
   return (
     <div className="card px-1 py-1">
       <Link to={`/products/${_id}`}>
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -23,7 +23,7 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
 
   useEffect(() => {
     if (products.length) {
@@ -37,37 +37,56 @@ function Detail() {
   }, [products, data, dispatch, id]);
 
   const addToCart = () => {
+    const itemInCart = cart.find(cartItem => cartItem._id === id);
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 },
+      });
+    }
+  };
+
+  const removeFromCart = () => {
     dispatch({
-      type: ADD_TO_CART,
-      product: { ...currentProduct, purchaseQuantity: 1 },
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id,
     });
   };
 
   return (
     <>
-      <Cart>
-        {currentProduct ? (
-          <div className="container my-1">
-            <Link to="/">← Back to Products</Link>
+      {currentProduct && cart ? (
+        <div className="container my-1">
+          <Link to="/">← Back to Products</Link>
 
-            <h2>{currentProduct.name}</h2>
+          <h2>{currentProduct.name}</h2>
 
-            <p>{currentProduct.description}</p>
+          <p>{currentProduct.description}</p>
 
-            <p>
-              <strong>Price:</strong>${currentProduct.price}{' '}
-              <button onClick={addToCart}>Add to Cart</button>
-              <button>Remove from Cart</button>
-            </p>
+          <p>
+            <strong>Price:</strong>${currentProduct.price}{' '}
+            <button onClick={addToCart}>Add to Cart</button>
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
+              onClick={removeFromCart}>
+              Remove from Cart
+            </button>
+          </p>
 
-            <img
-              src={`/images/${currentProduct.image}`}
-              alt={currentProduct.name}
-            />
-          </div>
-        ) : null}
-        {loading ? <img src={spinner} alt="loading" /> : null}
-      </Cart>
+          <img
+            src={`/images/${currentProduct.image}`}
+            alt={currentProduct.name}
+          />
+        </div>
+      ) : null}
+      {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
