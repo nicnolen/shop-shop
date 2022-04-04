5f648fc92d938fa8b4fcf0966736594494f7ad74
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 12:30:08 -0500

    added test to remove item from cart

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -100,3 +100,29 @@ test('ADD_MULTIPLE_TO_CART', () => {
   expect(newState.cart.length).toBe(4);
   expect(initialState.cart.length).toBe(2);
 });
+
+//! Test to remove item from cart
+test('REMOVE_FROM_CART', () => {
+  let newState1 = reducer(initialState, {
+    type: REMOVE_FROM_CART,
+    _id: '1',
+  });
+
+  //* cart is still open
+  expect(newState1.cartOpen).toBe(true);
+
+  //* the second item should now be the first
+  expect(newState1.cart.length).toBe(1);
+  expect(newState1.cart[0]._id).toBe('2');
+
+  let newState2 = reducer(newState1, {
+    type: REMOVE_FROM_CART,
+    _id: '2',
+  });
+
+  //* cart is empty and closed
+  expect(newState2.cartOpen).toBe(false);
+  expect(newState2.cart.length).toBe(0);
+
+  expect(initialState.cart.length).toBe(2);
+});
