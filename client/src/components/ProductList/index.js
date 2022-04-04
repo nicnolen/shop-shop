0de9216186c5e54cec8d6fbda5cd1c7998efc5f7
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 11:36:13 -0500

    updated Product List component to use and update global state

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,8 @@
 //TODO: PRODUCT LIST COMPONENT DISPLAYS PRODUCTS FROM APOLLO QUERY
 //! Import dependencies
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 
 import ProductItem from '../ProductItem';
@@ -8,23 +10,36 @@ import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
 //! Create ProductList component
-function ProductList({ currentCategory }) {
+function ProductList() {
+  const [state, dispatch] = useStoreContext();
+
+  const { currentCategory } = state;
+
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+    }
+  }, [data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return products;
+      return state.products;
     }
 
-    return products.filter(product => product.category._id === currentCategory);
+    return state.products.filter(
+      product => product.category._id === currentCategory
+    );
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map(product => (
             <ProductItem
