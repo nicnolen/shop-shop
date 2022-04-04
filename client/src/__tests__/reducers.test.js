686da90b2177624efd68567a133b82705db3e204
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 09:44:42 -0500

    added test to update the categories array

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -19,7 +19,9 @@ const initialState = {
 //! Test for updating products list
 test('UPDATE_PRODUCTS', () => {
   let newState = reducer(initialState, {
+    //* execute the UPDATE_PRODUCTS action
     type: UPDATE_PRODUCTS, // Type of action we are performing (predefined action)
+    //* update products list to be a new array of products
     products: [{}, {}], // Value (name) representing the new data you want to use with the action
   });
 
@@ -28,3 +30,18 @@ test('UPDATE_PRODUCTS', () => {
   //* check to make sure initial state didnt change
   expect(initialState.products.length).toBe(0);
 });
+
+//! Test for updating the categories array
+test('UPDATE_CATEGORIES', () => {
+  let newState = reducer(initialState, {
+    //* execute the UPDATE_CATEGORIES action
+    type: UPDATE_CATEGORIES,
+    //* update categories list to be a new array of categories
+    categories: [{}, {}]
+  });
+  
+  //* check to make sure categories were added to the new state
+  expect(newState.categories.length).toBe(2);
+  //* check to make sure initial state didnt change
+  expect(initialState.categories.length).toBe(1);
+})
