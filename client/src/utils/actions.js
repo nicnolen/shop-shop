e6f7d2fee36c06583a830ba0992e26ea4ef923b5
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 12:10:29 -0500

    added actions to perform with a shopping cart in actions.js file

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -7,3 +7,10 @@ export const UPDATE_PRODUCTS = 'UPDATE PRODUCTS';
 export const UPDATE_CATEGORIES = 'UPDATE CATEGORIES';
 //* Select a category from the state created by UPDATE_CATEGORIES action and display products for that category from the list created by the UPDATE_PRODUCTS action
 export const UPDATE_CURRENT_CATEGORY = 'UPDATE CURRENT_CATEGORY';
+export const ADD_TO_CART = 'ADD TO CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD MULTIPLE TO CART';
+export const REMOVE_FROM_CART = 'REMOVE FROM CART';
+export const UPDATE_CART_QUANTITY = 'UPDATE CART QUANTITY';
+export const CLEAR_CART = 'CLEAR CART';
+export const TOGGLE_CART = 'TOGGLE_CART';
+
