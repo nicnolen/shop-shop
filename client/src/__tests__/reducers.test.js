7902751ea321c7f666ec1cf4a822c0cdeb28ddec
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 09:27:09 -0500

    added a test for updating products list

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,27 @@
+//TODO: TEST REDUCER FUNCTIONALITY
+//! Import actions
+import {
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from '../utils/actions';
+
+//! Create a sample of what global state will look like
+const initialState = {
+  products: [],
+  categories: [{ name: 'Food' }],
+  currentCategory: '1', // refers to the index of the categories array
+};
+
+//! Test for updating products list
+test('UPDATE_PRODUCTS', () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_PRODUCTS, // Type of action we are performing (predefined action)
+    products: [{}, {}], // Value (name) representing the new data you want to use with the action
+  });
+
+  //* check to make sure products were added to the new state
+  expect(newState.products.length).toBe(2); 
+  //* check to make sure initial state didnt change
+  expect(initialState.products.length).toBe(0);
+});
