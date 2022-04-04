d46412821ea94f06580fa839b9464b5df76faf14
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 10:59:32 -0500

    managed and updated state

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -6,3 +6,16 @@ import { useProductReducer } from './reducers';
 //! Create the global state object
 const StoreContext = createContext();
 const { Provider } = StoreContext; //? Every Context object comes with a Provide and Consumer. Provider makes state data passed in available to all components. Consumer grabs and uses the Provider data
+
+//! Function to manage and update state
+const StoreProvider = ({ value = [], ...props }) => {
+  //* create initial global state
+  //? state: most up-to date version of global state object. dispatch: method to update our state
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: '',
+  });
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
