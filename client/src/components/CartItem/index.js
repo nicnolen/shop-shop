4e57be4c4ebfb359825f4e12c1fa047a8fd0280c
Nicholas Nolen <nicnolen@ymail.com>
2022-04-04 13:48:26 -0500

    added ability for trash can icon in shopping cart to remove items

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,33 +1,38 @@
 import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 const CartItem = ({ item }) => {
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = item => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id,
+    });
+  };
   return (
     <div className="flex-row">
       <div>
-        <img
-          src={`/images/${item.image}`}
-          alt=""
-        />
+        <img src={`/images/${item.image}`} alt="" />
       </div>
       <div>
-        <div>{item.name}, ${item.price}</div>
+        <div>
+          {item.name}, ${item.price}
+        </div>
         <div>
           <span>Qty:</span>
-          <input
-            type="number"
-            placeholder="1"
-            value={item.purchaseQuantity}
-          />
+          <input type="number" placeholder="1" value={item.purchaseQuantity} />
           <span
             role="img"
             aria-label="trash"
-          >
+            onClick={() => removeFromCart(item)}>
             🗑️
           </span>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default CartItem;
