27224833a1b9e485b2df74ab612edef30459340c
Nicholas Nolen <nicnolen@ymail.com>
2022-04-07 09:22:03 -0500

    saved product data to IndexedDB object store

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,7 @@
 //TODO: PRODUCT LIST COMPONENT DISPLAYS PRODUCTS FROM APOLLO QUERY
 //! Import dependencies
 import React, { useEffect } from 'react';
+import { idbPromise } from '../../utils/helpers';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
@@ -18,13 +19,20 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
+    //* if theres data stored
     if (data) {
+      // store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+
+      // but let's also take each product and save it to IndexedDB using the helper function
+      data.products.forEach(product => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
