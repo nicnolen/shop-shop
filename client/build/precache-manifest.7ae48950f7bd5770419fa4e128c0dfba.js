6d68a3acc32db74fd69e91580006f72832485457
Nicholas Nolen <nicnolen@ymail.com>
2022-04-07 14:54:11 -0500

    created an optimized production build

---
+++ client/build/precache-manifest.7ae48950f7bd5770419fa4e128c0dfba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa7261cf47b563ecef66784e2c638ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d67c601e783689eb6425",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "24c40cc5354616da34f8",
+    "url": "/static/js/2.9a5f0e5c.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.9a5f0e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d67c601e783689eb6425",
+    "url": "/static/js/main.cf33449a.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
