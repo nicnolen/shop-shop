53bcdb82be86668ab9eecde740e53ba0f451b613
Nicholas Nolen <nicnolen@ymail.com>
2022-04-07 13:09:13 -0500

    removed items from IndexedDB when an item is removed from cart

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { idbPromise } from '../../utils/helpers';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
@@ -10,6 +11,7 @@ const CartItem = ({ item }) => {
       type: REMOVE_FROM_CART,
       _id: item._id,
     });
+    idbPromise('cart', 'delete', { ...item });
   };
 
   const onChange = e => {
@@ -20,12 +22,16 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id,
       });
+
+      idbPromise('cart', 'delete', { ...item });
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value),
       });
+
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
     }
   };
 
